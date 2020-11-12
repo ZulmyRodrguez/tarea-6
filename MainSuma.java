8bd38f124948240dafc7b706bef00d7906360136
ZulmyRodrguez <74334021+ZulmyRodrguez@users.noreply.github.com>
2020-11-11 21:28:50 -0600

    Tarea1

---
+++ MainSuma.java
@@ -0,0 +1,37 @@
+
+package com.mycompany.ejercicio1;
+
+
+/**
+ *
+ * @author Latitude 3330
+ */
+public class MainSuma {
+    
+
+    
+    public static void main(String[] args) {
+        
+       double suma=0,resta=0, multiplicacion=0,division=0;
+       
+       double numero1=5;
+       double numero2=7;
+       
+       suma= numero1 + numero2;
+       resta= numero1 - numero2;
+       multiplicacion= numero1 * numero2;
+       
+       if (numero2 != 0){
+           division= numero1 / numero2;
+           
+       }
+ System.out.println("La suma es: " + suma);
+ System.out.println("La resta es: " + resta);
+ System.out.println("La multiplicacion es: " + multiplicacion);
+ System.out.println("La division es: " + division);
+              
+       
+       
+    }
+    
+}
